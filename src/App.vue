709a2bded37163e5be2192e5446aef2f84da2dba
Nikita Avdeev <nikus.master@gmail.com>
2023-06-03 19:11:55 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<Layout/>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue"
+
+export default {
+	components: {
+		Layout: defineAsyncComponent({
+			loader: () => import("/views/Layout.vue"),
+		})
+	},
+}
+</script>
+
+<style></style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,17 @@
+<script>
+import {defineComponent} from 'vue'
+import Header from "../components/Header.vue";
+
+export default defineComponent({
+	name: "Layout",
+	components: {Header}
+})
+</script>
+
+<template>
+	<Header/>
+</template>
+
+<style scoped>
+
+</style>
