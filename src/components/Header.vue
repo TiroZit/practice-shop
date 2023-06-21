04572c5d11b4d576801d4b0ecc016199ac8db675
Nikita Avdeev <nikus.master@gmail.com>
2023-06-21 13:26:15 +0500

    fix(Header) поправил

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -155,14 +155,6 @@
 								<a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
 							</div>
 
-							<div class="hidden lg:ml-8 lg:flex">
-								<a href="#" class="flex items-center text-gray-700 hover:text-gray-800">
-									<img src="https://tailwindui.com/img/flags/flag-canada.svg" alt="" class="block h-auto w-5 flex-shrink-0" />
-									<span class="ml-3 block text-sm font-medium">CAD</span>
-									<span class="sr-only">, change currency</span>
-								</a>
-							</div>
-
 							<!-- Search -->
 							<div class="flex lg:ml-6">
 								<a href="#" class="p-2 text-gray-400 hover:text-gray-500">
